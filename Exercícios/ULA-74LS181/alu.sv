cf8a1c438faa22298dfdb287307403dd81ac9eac
joaosoaresaalm <joao.almeida@academico.ifpb.edu.br>
2019-05-20 20:25:19 -0300

    Update alu.sv

--- Exercícios/ULA-74LS181/alu.sv
+++ Exercícios/ULA-74LS181/alu.sv
@@ -29,22 +29,22 @@ always_comb begin
         endcase
     end else begin
         case (s)
-            4'b0000 : f = a - 1 + c_in;
-          	4'b0001 : f = (a & b) - 1 + c_in;
-          	4'b0010 : f = (a & ~b) - 1 + c_in;
-            4'b0011 : f = -1 + c_in;
-          	4'b0100 : f = a + (a + ~b) + c_in;
-          	4'b0101 : f = (a & b) + (a + ~b) + c_in;
+            4'b0000 : f = a - 1;
+          	4'b0001 : f = (a & b) - 1;
+          	4'b0010 : f = (a & ~b) - 1;
+            4'b0011 : f = -1;
+            4'b0100 : f = a + (a | ~b);
+          	4'b0101 : f = (a & b) + (a | ~b);
             4'b0110 : f = a - b - 1 + c_in;
-          	4'b0111 : f = (a + ~b) + c_in;
-          	4'b1000 : f = a + (a + b) + c_in;
-            4'b1001 : f = a + b + c_in;
-          	4'b1010 : f = (a & ~b) + (a + b)+ c_in;
+          	4'b0111 : f = (a | ~b) + c_in;
+          	4'b1000 : f = a + (a | b) + c_in;
+            4'b1001 : f = a + b;
+            4'b1010 : f = (a & ~b) + (a | b);
             4'b1011 : f = a + b + c_in;
-            4'b1100 : f = a + ( a << 1) + c_in;
-          	4'b1101 : f = (a & b) + a + c_in;
-          	4'b1110 : f = (a & ~b) - a + c_in;
-            4'b1111 : f = a + c_in;
+            4'b1100 : f = a + ( a << 1); 
+            4'b1101 : f = (a & b) + a;
+            4'b1110 : f = (a & ~b) - a; 
+            4'b1111 : f = a; 
             default : f = '0;
         endcase
     end
